e78fab3b22b42ca56b8f0141a83fd6abd305a810
Enrique Maldonado <enriquemromo@rfpro.mx>
2020-07-22 21:11:26 -0500

    Servicios de prueba

--- src/main/java/mx/rfpro/delivery/controller/CategoryController.java
+++ src/main/java/mx/rfpro/delivery/controller/CategoryController.java
@@ -1,16 +1,17 @@
 package mx.rfpro.delivery.controller;
 
-import org.springframework.web.bind.annotation.RestController;
-
 import java.util.ArrayList;
 import java.util.List;
 import mx.rfpro.delivery.model.Category;
-
+import mx.rfpro.delivery.model.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
 
 @RestController
 @RequestMapping("v1/api/support")
@@ -37,6 +38,17 @@ public class CategoryController{
 		} catch (Exception ex) {
 			return ResponseEntity.status(500).body(ex.getMessage());
 		}
-	}
+    }
+    
+    @PostMapping
+    public ResponseEntity sigup(@RequestBody User user) {
+
+      try{
+        return ResponseEntity.ok(user);
+      } catch (Exception ex) {
+        return ResponseEntity.status(500).body(ex.getMessage());
+      }
+
+    }
 
 }
